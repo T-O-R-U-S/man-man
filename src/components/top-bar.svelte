0207fffd69e6360ab218d3d989925d28a09c0a82
T•Ø•R•Ü•S <bageliq@protonmail.com>
2023-10-09 10:23:29 +0400

    Fix UI bug, fix error in .gitignore

--- src/components/top-bar.svelte
+++ src/components/top-bar.svelte
@@ -19,15 +19,15 @@
                         </ul>
                     </div>
                 </section>
-                <a href="/doctor/schedule" class="p-4 outline outline-black outline-2 rounded-lg col-span-1 flex items-center justify-center flex-col">
+                <a href="/doctor/schedule" class="p-4 outline outline-black outline-2 rounded-lg col-span-1 flex items-center justify-center flex-col text-center">
                         <div class="bg-slate-600 aspect-square rounded-full flex-1 flex justify-center items-center fill-white">
                             <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><path fill="currentColour" d="M12 14q-.425 0-.713-.288T11 13q0-.425.288-.713T12 12q.425 0 .713.288T13 13q0 .425-.288.713T12 14Zm-4 0q-.425 0-.713-.288T7 13q0-.425.288-.713T8 12q.425 0 .713.288T9 13q0 .425-.288.713T8 14Zm8 0q-.425 0-.713-.288T15 13q0-.425.288-.713T16 12q.425 0 .713.288T17 13q0 .425-.288.713T16 14Zm-4 4q-.425 0-.713-.288T11 17q0-.425.288-.713T12 16q.425 0 .713.288T13 17q0 .425-.288.713T12 18Zm-4 0q-.425 0-.713-.288T7 17q0-.425.288-.713T8 16q.425 0 .713.288T9 17q0 .425-.288.713T8 18Zm8 0q-.425 0-.713-.288T15 17q0-.425.288-.713T16 16q.425 0 .713.288T17 17q0 .425-.288.713T16 18ZM5 22q-.825 0-1.413-.588T3 20V6q0-.825.588-1.413T5 4h1V2h2v2h8V2h2v2h1q.825 0 1.413.588T21 6v14q0 .825-.588 1.413T19 22H5Zm0-2h14V10H5v10Z"/></svg>
                         </div>
                         Open Complete Schedule
                 </a>
-                <section class="p-4 outline outline-black outline-2 rounded-lg col-span-1 flex items-center justify-center flex-col cursor-pointer">
+                <section class="p-4 outline outline-black outline-2 rounded-lg col-span-1 flex items-center justify-center flex-col cursor-pointer text-center">
                     <div class="bg-slate-600 aspect-square rounded-full flex-1 flex justify-center items-center fill-white">
-
+                        <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><path fill="currentColor" d="M12 4.5C7 4.5 2.73 7.61 1 12c1.73 4.39 6 7.5 11 7.5s9.27-3.11 11-7.5c-1.73-4.39-6-7.5-11-7.5zM12 17c-2.76 0-5-2.24-5-5s2.24-5 5-5s5 2.24 5 5s-2.24 5-5 5zm0-8c-1.66 0-3 1.34-3 3s1.34 3 3 3s3-1.34 3-3s-1.34-3-3-3z"/></svg>
                     </div>
                     Turn on Blind Mode
                 </section>
